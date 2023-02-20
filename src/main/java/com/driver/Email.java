3d8080ea19d67c48498f264a0208c4591cbc1798
NagarjunaReddy43 <perlanagarjunareddy47@gmail.com>
2023-02-20 16:19:58 +0530

    Nagarjuna

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -1,10 +1,8 @@
 package com.driver;
 
-import static com.google.common.net.HostSpecifier.isValid;
-
 public class Email {
 
-    protected String emailId;
+    private String emailId;
     private String password;
 
     public Email(String emailId){
@@ -27,37 +25,47 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
-        if(oldPassword.equals(password)) {
-            if (isValid(newPassword)) {
-                System.out.println("Password Changed Successfully!");
+
+        if(oldPassword.equals(password)){
+            if(isValid(newPassword)){
+                System.out.println("Password changed successfully!");
                 this.password = newPassword;
             }
-            else {
-                System.out.println("The Password is not valid!");
+            else{
+                System.out.println("The new password is not valid!");
             }
         }
         else{
             System.out.println("The given password does not match current password!");
         }
     }
-    public static  boolean isValid(String passw){
-        boolean isUppercase = false;
-        boolean isLowercase = false;
-        boolean isSpecial = false;
-        boolean isdigit = false;
-        String specialCharactersString = "!@#$%&*()'+,-./:;<=>?[]^_`{|}";
-        if(passw.length()<8) return false;
 
-        for(int i=0;i<passw.length();i++){
-            if(passw.charAt(i)>='A' && passw.charAt(i)<='Z') isUppercase = true;
+    private Boolean isValid(String password){
+        Boolean capitalLetter = false;
+        Boolean smallLetter = false;
+        Boolean digit = false;
+        Boolean specialCharacter = false;
 
-            if(passw.charAt(i)>='1' && passw.charAt(i)<='9') isdigit = true;
-
-            if(passw.charAt(i)>='a' && passw.charAt(i)<='z') isLowercase = true;
-
-            if(specialCharactersString.contains(String.valueOf(passw.charAt(i)))) isSpecial = true;
-
+        if(password.length() < 8){
+            return false;
         }
-        return isdigit && isSpecial && isUppercase && isLowercase;
+
+        for(int i = 0; i<password.length(); i++){
+            char ch = password.charAt(i);
+            if((ch >= 'A') && (ch <= 'Z')){
+                capitalLetter = true;
+            }
+            else if((ch >= 'a') && (ch <= 'z')){
+                smallLetter = true;
+            }
+            else if((ch >= '0') && (ch <= '9')){
+                digit = true;
+            }
+            else specialCharacter = true;
+        }
+
+        if(capitalLetter && smallLetter && digit && specialCharacter)
+            return true;
+        return false;
     }
 }
